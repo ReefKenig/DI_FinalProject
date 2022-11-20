650ecf73bda98a6817bad4a7b5cfb6093c878d2a
OshiroSama <rkenig@aol.com>
2022-11-20 23:21:25 +0200

    trying tetris

---
+++ tetris/app.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const grid = document.querySelector(".grid");
+  let squares = Array.from(document.querySelectorAll(".grid div"));
+  const scoreDisplay = document.querySelector("#score");
+  const startButton = document.querySelector("#start_button");
+  const width = 10;
+
+  const shapes = {
+    lTetromino: [
+      [1, width + 1, width * 2 + 1, 2],
+      [width, width + 1, width + 2, width * 2 + 2],
+      [1, width + 1, width * 2 + 1, width * 2],
+      [width, width * 2, width * 2 + 1, width * 2 + 2],
+    ],
+  };
+});
