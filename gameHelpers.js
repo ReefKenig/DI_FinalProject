4e95f1b39c6c058d3362184fc557adbb26fc6108
OshiroSama <70912976+OshiroSama@users.noreply.github.com>
2022-11-28 18:40:56 +0200

    Basic tetris game without score

---
+++ gameHelpers.js
@@ -0,0 +1,28 @@
+export const STAGE_WIDTH = 12;
+export const STAGE_HEIGHT = 20;
+
+export const createStage = () =>
+  Array.from(Array(STAGE_HEIGHT), () =>
+    new Array(STAGE_WIDTH).fill([0, "clear"])
+  );
+
+export const checkCollision = (player, stage, { x: moveX, y: moveY }) => {
+  for (let y = 0; y < player.tetromino.length; y++) {
+    for (let x = 0; x < player.tetromino[y].length; x++) {
+      // Make sure that this is a tetromino cell
+      if (player.tetromino[y][x] !== 0) {
+        // 1. Check that movement is inside stage height(y)
+        // 2. Check that movement is inside stage width(x)
+        // 3. Check that movement won't collide with another tetromino
+        if (
+          !stage[y + player.pos.y + moveY] ||
+          !stage[y + player.pos.y + moveY][x + player.pos.x + moveX] ||
+          stage[y + player.pos.y + moveY][x + player.pos.x + moveX][1] !==
+            "clear"
+        ) {
+          return true;
+        }
+      }
+    }
+  }
+};
